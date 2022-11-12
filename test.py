c2cf20dab8f29f02a35b2a91e74efae9257ee55e
JamesB69nd <118055320+JamesB69nd@users.noreply.github.com>
2022-11-12 12:59:17 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import requests
 
-open("test.py", "w").write(requests.get("https://raw.githubusercontent.com/Th3K1n91/workspace/main/test.py?token=GHSAT0AAAAAABUUBZ6TUUVN2SOJWD46HJUYY3PRDZQ").text)
+open("test.py", "w").write(requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").text)
+
+print("yes")
