879eb730265d0ccdc753a3b8ddafd5d818bdb766
JamesB69nd <118055320+JamesB69nd@users.noreply.github.com>
2022-11-12 13:38:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ print(len(update.strip().replace("\n", "")))
 print(len(open("test.py").read().strip()))
 if len(update.strip().replace("\n", "")) != len(open("test.py").read().strip()):
     print("Updating Code")
-    open("test.py", "w").write(update.strip().replace("\n", ""))
+    open("test.py", "w").write(update.strip())
     print("Done")
     print("Restart Code")
     exit()
