db68cc86739bcc8f08592419ac83c89d0fee43d8
JamesB69nd <118055320+JamesB69nd@users.noreply.github.com>
2022-11-12 13:09:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import requests
 
+print("Updating Code..")
 open("test.py", "w").write(requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").text)
+print("Update done")
+
 print("yes")
