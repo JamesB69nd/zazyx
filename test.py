1074dd022d2c1c7a65648b0a048f04c70f342641
JamesB69nd <118055320+JamesB69nd@users.noreply.github.com>
2022-11-12 13:32:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-import requestsupdate = requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").textprint(len(update.strip().replace("\n", "")))print(len(open("test.py").read().strip()))if len(update.strip().replace("\n", "")) != len(open("test.py").read().strip()):    print("Updating Code")    open("test.py", "w").write(update.strip().replace("\n", ""))    print("Done")    print("Restart Code")    exit()
+import requests
+
+update = requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").text
+print(len(update.strip().replace("\n", "")))
+print(len(open("test.py").read().strip()))
+if len(update.strip().replace("\n", "")) != len(open("test.py").read().strip()):
+    print("Updating Code")
+    open("test.py", "w").write(update.strip().replace("\n", ""))
+    print("Done")
+    print("Restart Code")
+    exit()
+print("yes")
