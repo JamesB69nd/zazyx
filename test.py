b35a89eaf7726ffc8b2269e3bdb527b3a1127e83
JamesB69nd <118055320+JamesB69nd@users.noreply.github.com>
2022-11-12 13:20:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
 
 update = requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").text
-print(len(update))
-print(len(open("test.py").read()))
-if len(update) != len(open("test.py").read()):
+print(len(update.strip().replace("\n", "")))
+print(len(open("test.py").read().strip()))
+if len(update.strip().replace("\n", "")) != len(open("test.py").read().strip()):
     print("Updating Code")
-    open("test.py", "w").write(update)
+    open("test.py", "w").write(update.strip().replace("\n", ""))
     print("Done")
     print("Restart Code")
     exit()
