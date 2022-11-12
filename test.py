4cd9964514c2d9770870ff9a9e3005c190e81783
JamesB69nd <118055320+JamesB69nd@users.noreply.github.com>
2022-11-12 13:12:43 +0100

    test

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import requests
 
-print("Updating Code..")
-open("test.py", "w").write(requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").text)
-print("Update done")
-
+update = requests.get("https://raw.githubusercontent.com/JamesB69nd/zazyx/main/test.py").text
+if len(update) != len(open("test.py").read()):
+    print("Updating Code")
+    open("test.py", "w").write(update)
+    print("Done")
+    print("Restart Code")
+    exit()
 print("yes")
+
